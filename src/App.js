c4155e221ffb9e70e1ed8b19dc46db2afb29edd9
Prism <ilex.laurel.smith@gmail.com>
2022-05-29 14:44:26 +0100

    Added isLoading state and a spinner component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
 import arraySort from "array-sort";
 import { useEffect, useState } from "react";
+import Spinner from "./components/spinner/spinner.component";
 import UserList from "./components/user-list/user-list.component";
 
 function App() {
   const [users, setUsers] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
   const url = 'https://randomuser.me/api/?results=1000&seed=chalkboard&inc=name,dob'
 
   useEffect(() => {
+    setIsLoading(true);
     fetch(url)
       .then(response => response.json())
       .then(data => {
-        const orderedUsers = arraySort(data.results, 'name.first')
+        const orderedUsers = arraySort(data.results, 'name.first');
         setUsers(orderedUsers);
+        setIsLoading(false);
       })
   }, [])
 
   return (
     <div className="grid place-items-center">
       <h1 className="py-6 text-4xl">Birthdays</h1>
-      <UserList users={users} />
+      {isLoading
+        ? <Spinner />
+        : <UserList users={users} />
+      }
     </div>
   );
 }
