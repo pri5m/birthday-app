b77787d38405708c75418b613cee22df4556b3be
Prism <ilex.laurel.smith@gmail.com>
2022-05-29 14:19:08 +0100

    List and card styling and user avatar

--- src/components/user-card/user-card.component.jsx
+++ src/components/user-card/user-card.component.jsx
@@ -1,13 +1,18 @@
 import { Fragment } from "react";
+import Avatar from "react-avatar";
 
 const UserCard = ({ user }) => {
     const { name, dob } = user;
+    const fullname = name.first + ' ' + name.last;
 
     return (
-        <Fragment>
-            <h2>{name.first}</h2>
-            <p>{dob.date}</p>
-        </Fragment>
+        <div className='grid col-8 gap-2 px-6 py-2 my-1 bg-slate-100 rounded-md'>
+            <Avatar name={fullname} size="50" textSizeRatio={0.5} round={true} />
+            <div className="col-start-2 col-span-5">
+                <h2 className="font-bold">{name.first}</h2>
+                <p>{dob.date}</p>
+            </div>
+        </div>
     );
 };
 
--- src/components/user-list/user-list.component.jsx
+++ src/components/user-list/user-list.component.jsx
@@ -2,11 +2,11 @@ import { Fragment } from "react";
 import UserCard from "../user-card/user-card.component";
 
 const UserList = ({ users }) => (
-    <Fragment>
+    <div className='p-5 bg-slate-300 rounded-md'>
         {users.map((user) => {
             return <UserCard user={user} />;
         })}
-    </Fragment>
+    </div>
 );
 
 export default UserList;
