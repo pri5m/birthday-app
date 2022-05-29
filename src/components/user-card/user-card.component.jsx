ecf120557895c98270abf6e741db45b071c98d3c
Prism <ilex.laurel.smith@gmail.com>
2022-05-29 14:30:18 +0100

    Added date formatter into card component

--- src/components/user-card/user-card.component.jsx
+++ src/components/user-card/user-card.component.jsx
@@ -1,16 +1,18 @@
 import { Fragment } from "react";
 import Avatar from "react-avatar";
+import { useDateFormatter } from "../../hooks/useDateFormatter";
 
 const UserCard = ({ user }) => {
     const { name, dob } = user;
+    const date = useDateFormatter(dob.date);
     const fullname = name.first + ' ' + name.last;
 
     return (
         <div className='grid col-8 gap-2 px-6 py-2 my-1 bg-slate-100 rounded-md'>
             <Avatar name={fullname} size="50" textSizeRatio={0.5} round={true} />
             <div className="col-start-2 col-span-5">
-                <h2 className="font-bold">{name.first}</h2>
-                <p>{dob.date}</p>
+                <h2 className="font-bold">{fullname}</h2>
+                <p>{date}</p>
             </div>
         </div>
     );
