fd33caecfd006dc3857b2b244a7ca0c1c8a556f4
Prism <ilex.laurel.smith@gmail.com>
2022-05-29 15:14:57 +0100

    Added home route

--- src/App.js
+++ src/App.js
@@ -1,30 +1,13 @@
-import arraySort from "array-sort";
-import { useEffect, useState } from "react";
-import Spinner from "./components/spinner/spinner.component";
-import UserList from "./components/user-list/user-list.component";
-import { useFetch } from "./hooks/useFetch";
+import { Route, Routes } from "react-router-dom";
+import Home from "./routes/home/home.component";
+
 
 function App() {
-  const url = 'https://randomuser.me/api/?results=1000&seed=chalkboard&inc=name,dob,login'
-  const [orderedUsers, setOrderedUsers] = useState([]);
-  const { data, error, isLoading } = useFetch(url);
-
-  useEffect(() => {
-    // @ts-ignore
-    const results = data.results;
-    setOrderedUsers(arraySort(results, 'name.first'));
-  }, [data])
-
   return (
-    <div className="grid place-items-center">
-      <h1 className="py-6 text-4xl" role="home-title">Birthdays</h1>
-      {error && <div className='my-8'>{error}</div>}
-      {isLoading
-        ? <Spinner />
-        : <UserList users={orderedUsers} />
-      }
-    </div>
-  );
+    <Routes>
+      <Route path='/' element={<Home />} />
+    </Routes>
+  )
 }
 
 export default App;
